affb690a35bebc6416f59828056e8313b999bbab
anthonycassol <anthonylcassol@gmail.com>
2020-08-13 15:31:31 -0300

    conexao https to http

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -13,8 +13,6 @@ module.exports = {
 
         const query = "SELECT idusuarios, nome, sobrenome, email, status FROM usuarios WHERE email = ? AND senha = ?";
 
-        console.log(query);
-
         db.all(query, [email, senha], (err, rows) => {
             if(err){
                 console.log(err.message);
